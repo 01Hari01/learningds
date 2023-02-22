625ac7d2f8d8e480253f638e71fdf4c4c4b6c1a5
01Hari01 <harikrishnathoka@gmail.com>
2023-02-22 11:16:05 +0000

    initial commit

---
+++ ADT/lists.py
@@ -0,0 +1,16 @@
+#This file is dedicarted for the purpose of understnding the lists concepts step by step in python 3.
+#Author: HariKrishna Thoka
+#Date Created: 21-02-2023
+
+numbers=[1,2,6,5,545,454,34]
+total=sum(numbers)
+sizze=len(numbers)
+numbers.extend((24,43,232))
+numbers.insert(4,54)
+print(numbers,total,sizze)
+
+#Learning point 1: Most important functions on lists are append, extend, insert, pop, remove, reverse, sort
+
+
+
+
